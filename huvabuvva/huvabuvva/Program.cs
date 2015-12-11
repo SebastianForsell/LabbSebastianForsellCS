483ea2ef58dddddc0084cf643d0130643d43fd35
SebastianForsell <sebastian@brantevik.net>
2015-12-11 17:36:41 +0100

    The very start

--- huvabuvva/huvabuvva/Program.cs
+++ huvabuvva/huvabuvva/Program.cs
@@ -10,7 +10,29 @@ namespace huvabuvva
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("I A.M.");
+            Animal cat = new Cat() { Name = "Kajsa", Age = 42};
+            //cat.PrintInfo();
+            Console.WriteLine(cat);
+            Animal dog = new Dog() { Name = "Dogge", Age = 666};
+            //dog.PrintInfo();
+            Console.WriteLine(dog);
+            //Vehicle vehicle = new Car();
+            Console.ReadLine();
+            
+            //Cykel cykel = new Cykel();
+            //Fordon fordon = (Fordon)cykel;
+            //Bil bil = new Bil();
+            //Fordon[] fordonare = new Fordon[3] { cykel, fordon, bil};
+            //foreach (var item in fordonare)
+            //{
+            //    item.print();
+            //}
+            //fordonare[0] = cykel;
+            //fordonare[1] = fordon;
+            //fordonare[2] = bil;
+            //cykel.print();
+            //fordon.print();
+            Console.ReadLine();
         }
     }
 }
