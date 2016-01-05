c10b71889f8cdb9e73082dc5f75502b6705b099f
SebastianForsell <sebastian@brantevik.net>
2016-01-05 10:33:07 +0100

    Exercis1 DONE!

---
+++ Webbforms_Exercises/Exercise1/WebForm1.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Exercise1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <p>Simple Weight Converter</p>
+    <form id="form1" runat="server">
+        <asp:ListBox ID="fromList" runat="server" Height="76px" Width="74px">
+            <asp:ListItem Value="gram">Gram</asp:ListItem>
+            <asp:ListItem Value="hekto">Hektogram</asp:ListItem>
+            <asp:ListItem Value="kilo">Kilogram</asp:ListItem>
+            <asp:ListItem Value="milli">Milligram</asp:ListItem>
+        </asp:ListBox>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; --&gt;
+        <asp:ListBox ID="toList" runat="server" Height="76px" style="margin-left: 35px" Width="74px">
+            <asp:ListItem Value="gram">Gram</asp:ListItem>
+            <asp:ListItem Value="hekto">Hektogram</asp:ListItem>
+            <asp:ListItem Value="kilo">Kilogram</asp:ListItem>
+            <asp:ListItem Value="milli">Milligram</asp:ListItem>
+        </asp:ListBox>
+    <div>
+    
+    </div>
+        <p>
+            <asp:TextBox ID="weightInput" runat="server"></asp:TextBox>
+        </p>
+        <asp:Button ID="convertButton" runat="server" Font-Bold="True" ForeColor="Maroon" Height="54px" OnClick="Button1_Click" Text="Convert!" Width="131px" />
+        <p>
+            <asp:Label ID="convertResult" runat="server" Text="Resultat:"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ Webbforms_Exercises/Exercise1/WebForm1.aspx.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Exercise1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int weight = int.Parse(weightInput.Text);
+            Converter();
+        }
+        private void Converter()
+        {
+            switch (fromList.SelectedValue)
+            {
+                case "gram":
+                    double gram = double.Parse(weightInput.Text);
+                    backConverter(gram);
+                    break;
+                case "hekto":
+                    double hekto = double.Parse(weightInput.Text);
+                    hekto *= 100;
+                    backConverter(hekto);
+                    break;
+                case "kilo":
+                    double kilo = double.Parse(weightInput.Text);
+                    kilo *= 1000;
+                    backConverter(kilo);
+                    break;
+                case "milli":
+                    double milli = double.Parse(weightInput.Text);
+                    milli /= 1000;
+                    backConverter(milli);
+                    break;
+                default:
+                    break;
+            }
+        }
+        private void backConverter(double weightValue)
+        {
+            switch (toList.SelectedValue)
+            {
+                case "gram":
+                    convertResult.Text = ToString(weightValue);
+                    break;
+                case "hekto":
+                    weightValue /= 100;
+                    convertResult.Text = ToString(weightValue);
+                    break;
+                case "kilo":
+                    weightValue /= 1000;
+                    convertResult.Text = ToString(weightValue);
+                    break;
+                case "milli":
+                    weightValue *= 1000;
+                    convertResult.Text = ToString(weightValue);
+                    break;
+                default:
+                    break;
+            }
+        }
+        private string ToString(double weightValue)
+        {
+            switch (toList.SelectedValue)
+            {
+                case "gram":
+                    return $"Resultat: {weightValue}g.";
+                case "hekto":
+                    return $"Resultat: {weightValue}hg";
+                case "kilo":
+                    return $"Resultat: {weightValue}kg";
+                case "milli":
+                    return $"Resultat: {weightValue}mg";
+                default:
+                    throw new Exception("hoppsan, där gick något fel ... (ToString switch)");
+            }
+            
+        }
+    }
+}
