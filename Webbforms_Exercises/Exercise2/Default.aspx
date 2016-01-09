172215ca60c4f701d7f02aa7b85c219f3d1a7854
SebastianForsell <sebastian@brantevik.net>
2016-01-09 13:20:47 +0100

    Övning 2 AdventureWorks Finsished

---
+++ Webbforms_Exercises/Exercise2/Default.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Exercise2.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+        <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True">
+        </asp:DropDownList>
+        <p>
+            <asp:Label ID="Label1" runat="server" Text="" Visible="false"></asp:Label>
+        </p>
+        <asp:ListBox ID="ListBox1" runat="server" Height="393px" Width="437px"></asp:ListBox>
+    </form>
+</body>
+</html>
+
---
+++ Webbforms_Exercises/Exercise2/Default.aspx.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Exercise2
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection("Data Source=localhost; Initial Catalog=AdventureWorks2012; User ID=DaBaws; Password=gnu; Integrated Security=True");
+            string sqlQuery = "SELECT * FROM Production.ProductPhoto";
+            SqlCommand cmd = new SqlCommand(sqlQuery, connection);
+            SqlDataReader oreader;
+            try
+            {
+                ListItem lista1 = new ListItem();
+                lista1.Text = "Select";
+                lista1.Value = "0";
+                ListBox1.Items.Add(lista1);
+                connection.Open();
+                oreader = cmd.ExecuteReader();
+                while (oreader.Read())
+                {
+                    ListItem lista2 = new ListItem();
+                    lista2.Text = oreader["ThumbnailPhotoFileName"].ToString();
+                    lista2.Value = oreader["ProductPhotoID"].ToString();
+                    ListBox1.Items.Add(lista2);
+                }
+                oreader.Close();
+                oreader.Dispose();
+                cmd.Dispose();
+                connection.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Label1.Text = ex.Message;
+                Label1.Visible = true;
+                throw;
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection("Data Source=localhost; Initial Catalog=AdventureWorks2012; User ID=DaBaws; Password=gnu; Integrated Security=True");
+            string sqlQuery = "SELECT * FROM Production.ProductCategory";
+            SqlCommand cmd = new SqlCommand(sqlQuery, connection);
+            SqlDataReader oreader;
+            try
+            {
+                ListItem li = new ListItem();
+                li.Text = "Select";
+                li.Value = "0";
+                DropDownList1.Items.Add(li);
+                connection.Open();
+                oreader = cmd.ExecuteReader();
+                while (oreader.Read())
+                {
+                    ListItem lista2 = new ListItem();
+                    lista2.Text = oreader["Name"].ToString();
+                    lista2.Value = oreader["ProductCategoryID"].ToString();
+                    DropDownList1.Items.Add(lista2);
+                }
+                oreader.Close();
+                oreader.Dispose();
+                cmd.Dispose();
+                connection.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Label1.Text = ex.Message;
+                Label1.Visible = true;
+                throw;
+            }
+        }
+    }
+}
